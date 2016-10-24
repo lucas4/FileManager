33f5ef322f8986f0ff880621b7e85697166c026b
salce <salce@LAPTOP-8J8AGFLE>
2016-10-24 19:14:15 +0200

    history changes

--- FileManagerEngine/History.cs
+++ FileManagerEngine/History.cs
@@ -2,14 +2,59 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace FileManagerEngine
 {
-    partial class FileManager
+    partial class History : IHistory
     {
-        class History
-        {
+        ObservableCollection<DirectoryInfo> historyList = new ObservableCollection<DirectoryInfo>();
+        private int index = -1;
 
+
+        public void AddDirectory(DirectoryInfo directory)
+        {
+            historyList.Add(directory);
+            index = historyList.Count + 1;
+        }
+
+        public ObservableCollection<DirectoryInfo> GetHistory()
+        {
+            return historyList;
+        }
+
+        public int GetCurrentIndex()
+        {
+            return index;
+        }
+
+        public bool CanDirectoryGoBack()
+        {
+            if ((index - 1) >= 0)
+                return true;
+            else
+                return false;
+        }
+
+        public bool CanDirectoryGoForward()
+        {
+            if ((index + 1) <= historyList.Count - 1)
+                return true;
+            else
+                return false;
+        }
+
+        public DirectoryInfo DirectoryGoBack()
+        {
+            if (CanDirectoryGoBack()) index--;
+            return historyList[index];
+        }
+
+        public DirectoryInfo DirectoryGoForward()
+        {
+            if (CanDirectoryGoForward()) index++;
+            return historyList[index];
         }
     }
 }
